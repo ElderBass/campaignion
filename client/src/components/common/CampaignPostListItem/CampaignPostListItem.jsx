cd947c845fe1122306dc60b43694ebebb0f58f67
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2024-07-27 18:10:11 -0500

    Successfully adding posts and restyled how they appear.

--- client/src/components/common/CampaignPostListItem/CampaignPostListItem.jsx
+++ client/src/components/common/CampaignPostListItem/CampaignPostListItem.jsx
@@ -3,18 +3,23 @@ import dayjs from "../../../utils/dayjs";
 import styles from "./CampaignPostListItem.module.css";
 
 const CampaignPostListItem = ({ post }) => {
-	const { description, poster, dateAdded } = post;
+	const { title, description, poster, dateAdded } = post;
 	const date = dayjs(dateAdded).format("MMMM D, YYYY");
 
 	return (
 		<div className={styles.postListItem}>
-			<p>{description}</p>
-			<p className={styles.detail}>
-				Posted by: <span className={styles.span}>{poster}</span>
-			</p>
-			<p className={styles.detail}>
-				Date posted: <span className={styles.span}>{date}</span>
-			</p>
+			<p className={styles.title}>{title}</p>
+			<p className={styles.description}>{description}</p>
+			<div className={styles.deets}>
+				<div className={styles.deet}>
+					<p className={styles.detail}>Posted by:</p>
+					<p className={styles.span}>{poster}</p>
+				</div>
+				<div className={styles.deet}>
+					<p className={styles.detail}>Date posted:</p>
+					<p className={styles.span}>{date}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
